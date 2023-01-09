baf263b7de56a95c2bfeeb97300367a64daf3600
Guillaume MICHEL <guillaume@squaremind.io>
2023-01-09 16:36:20 +0100

    Cross compilation with protobuf dependency working

---
+++ conanfile.py
@@ -0,0 +1,15 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+class Test1Recipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps", "VirtualBuildEnv"
+
+    def requirements(self):
+        self.requires("protobuf/3.21.9")
+
+    def build_requirements(self):
+        self.tool_requires("protobuf/3.21.9")
+
+    def layout(self):
+        cmake_layout(self)
---
+++ src/test1.cpp
@@ -0,0 +1,5 @@
+#include <proto/animal.pb.h>
+
+int main() {
+    return 0;
+}
